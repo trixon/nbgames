bfd50cd1fc957c859669a1593d5336cec1a14603
Patrik Karlsson <patrik@trixon.se>
2013-12-09 15:41:14 +0100

    Empty Start Page

--- modules/org.nbgames.core/src/org/nbgames/core/Installer.java
+++ modules/org.nbgames.core/src/org/nbgames/core/Installer.java
@@ -1,6 +1,9 @@
 package org.nbgames.core;
 
+import java.util.prefs.Preferences;
+import org.nbgames.core.startpage.StartPageTopComponent;
 import org.openide.modules.ModuleInstall;
+import org.openide.util.NbPreferences;
 import org.openide.windows.TopComponent;
 import org.openide.windows.WindowManager;
 
@@ -10,14 +13,24 @@ import org.openide.windows.WindowManager;
  */
 public class Installer extends ModuleInstall {
 
+    private final Preferences mPreferences;
+
+    public Installer() {
+        mPreferences = NbPreferences.forModule(StartPageTopComponent.class);
+    }
+
     @Override
     public void restored() {
         WindowManager.getDefault().invokeWhenUIReady(new Runnable() {
             @Override
             public void run() {
+                if (mPreferences.getBoolean(StartPageTopComponent.KEY_SHOW_ON_STARTUP, true)) {
+                    openWindow("StartPageTopComponent");
+                }
                 openWindow("output");
             }
         });
+
         NbGames.log("Ready");
     }
 
