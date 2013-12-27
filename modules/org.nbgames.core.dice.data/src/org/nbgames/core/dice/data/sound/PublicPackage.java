e04e81cc3719cf633a842a4d7bc4cab8d6133b0a
Patrik Karlsson <patrik@trixon.se>
2013-12-27 18:45:24 +0100

    Made sound package public

---
+++ modules/org.nbgames.core.dice.data/src/org/nbgames/core/dice/data/sound/PublicPackage.java
@@ -0,0 +1,9 @@
+package org.nbgames.core.dice.data.sound;
+
+/**
+ * Dummy class that enables export of sound files.
+ *
+ * @author Patrik Karlsson <patrik@trixon.se>
+ */
+public class PublicPackage {
+}
