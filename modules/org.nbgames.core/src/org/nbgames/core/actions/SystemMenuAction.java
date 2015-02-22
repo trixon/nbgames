eab4c291708eb1be0fd5bd50f839483a3db0087d
Patrik Karlsson <patrik@trixon.se>
2015-02-22 15:52:01 +0100

    Display system menu on MouseDown

--- modules/org.nbgames.core/src/org/nbgames/core/actions/SystemMenuAction.java
+++ modules/org.nbgames.core/src/org/nbgames/core/actions/SystemMenuAction.java
@@ -15,8 +15,11 @@
  */
 package org.nbgames.core.actions;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -24,12 +27,15 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JSeparator;
+import javax.swing.SwingUtilities;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
 import org.openide.awt.Actions;
 import org.openide.awt.Mnemonics;
+import org.openide.util.ImageUtilities;
+import org.openide.util.actions.Presenter;
 import se.trixon.almond.dictionary.Dict;
 
 /**
@@ -41,58 +47,90 @@ import se.trixon.almond.dictionary.Dict;
         id = "org.nbgames.core.actions.SystemMenuAction"
 )
 @ActionRegistration(
-        iconBase = "org/nbgames/core/res/format-justify-fill.png",
-        displayName = "#CTL_SystemMenuAction"
+        displayName = "#CTL_SystemMenuAction", lazy = false
 )
 @ActionReferences({
     @ActionReference(path = "Toolbars/System", position = 9999),
     @ActionReference(path = "Shortcuts", name = "O-F10")
 })
-public final class SystemMenuAction implements ActionListener {
+public final class SystemMenuAction extends AbstractAction implements Presenter.Toolbar {
 
+    private final JButton mButton = new JButton();
     private final JPopupMenu mPopup = new JPopupMenu();
 
     public SystemMenuAction() {
-        JMenu menu;
+        SwingUtilities.invokeLater(() -> {
+            JMenu menu;
+            add(mPopup, "Window", "org.nbgames.core.StartPageTopComponent");
+            add(mPopup, "Window", "org.netbeans.core.windows.actions.ToggleFullScreenAction");
+            add(mPopup, "Window", "org.netbeans.core.windows.actions.ShowEditorOnlyAction");
+            mPopup.add(new JSeparator());
 
-        add(mPopup, "Window", "org.nbgames.core.StartPageTopComponent");
-        add(mPopup, "Window", "org.netbeans.core.windows.actions.ToggleFullScreenAction");
-        add(mPopup, "Window", "org.netbeans.core.windows.actions.ShowEditorOnlyAction");
-        mPopup.add(new JSeparator());
+            add(mPopup, "System", "org.netbeans.modules.autoupdate.ui.actions.PluginManagerAction");
+            add(mPopup, "Window", "org.netbeans.modules.options.OptionsWindowAction");
+            add(mPopup, "Window", "org.netbeans.core.windows.actions.ToolbarsListAction");
+            //FIXME Does not populate...
+            mPopup.add(new JSeparator());
 
-        add(mPopup, "System", "org.netbeans.modules.autoupdate.ui.actions.PluginManagerAction");
-        add(mPopup, "Window", "org.netbeans.modules.options.OptionsWindowAction");
-        add(mPopup, "Window", "org.netbeans.core.windows.actions.ToolbarsListAction");
-        //FIXME Does not populate...
-        mPopup.add(new JSeparator());
+            menu = new JMenu(Dict.SYSTEM.getString());
+            add(menu, "Window", "org.netbeans.core.windows.actions.ResetWindowsAction");
+            menu.add(new JSeparator());
+            add(menu, "Window", "org.netbeans.core.io.ui.IOWindowAction");
+            add(menu, "View", "org.netbeans.core.actions.LogAction");
+            mPopup.add(menu);
+            mPopup.add(new JSeparator());
 
-        menu = new JMenu(Dict.SYSTEM.getString());
-        add(menu, "Window", "org.netbeans.core.windows.actions.ResetWindowsAction");
-        menu.add(new JSeparator());
-        add(menu, "Window", "org.netbeans.core.io.ui.IOWindowAction");
-        add(menu, "View", "org.netbeans.core.actions.LogAction");
-        mPopup.add(menu);
-        mPopup.add(new JSeparator());
+            menu = new JMenu(Dict.HELP.getString());
+            add(menu, "Help", "org.nbgames.core.actions.HelpAction");
+            add(menu, "System", "org.netbeans.modules.autoupdate.ui.actions.CheckForUpdatesAction");
+            add(menu, "Help", "org.netbeans.core.actions.AboutAction");
+            mPopup.add(menu);
+            mPopup.add(new JSeparator());
 
-        menu = new JMenu(Dict.HELP.getString());
-        add(menu, "Help", "org.nbgames.core.actions.HelpAction");
-        add(menu, "System", "org.netbeans.modules.autoupdate.ui.actions.CheckForUpdatesAction");
-        add(menu, "Help", "org.netbeans.core.actions.AboutAction");
-        mPopup.add(menu);
-        mPopup.add(new JSeparator());
+            add(mPopup, "File", "se.trixon.almond.actions.QuitAction");
+        });
 
-        add(mPopup, "File", "se.trixon.almond.actions.QuitAction");
+        mButton.setIcon(ImageUtilities.loadImageIcon("org/nbgames/core/res/format-justify-fill24.png", false));
+        mButton.addMouseListener(new MouseListener() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    mPopup.show(mButton, mButton.getWidth() - mPopup.getWidth(), mButton.getHeight());
+
+                    int x = mButton.getLocationOnScreen().x + mButton.getWidth() - mPopup.getWidth();
+                    int y = mButton.getLocationOnScreen().y + mButton.getHeight();
+
+                    mPopup.setLocation(x, y);
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+        });
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        JButton button = (JButton) actionEvent.getSource();
-        mPopup.show(button, button.getWidth() - mPopup.getWidth(), button.getHeight());
+    }
 
-        int x = button.getLocationOnScreen().x + button.getWidth() - mPopup.getWidth();
-        int y = button.getLocationOnScreen().y + button.getHeight();
+    @Override
+    public Component getToolbarPresenter() {
 
-        mPopup.setLocation(x, y);
+        return mButton;
     }
 
     private void add(JComponent parentMenu, String category, String id) {
